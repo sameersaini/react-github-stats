3c6f7650b3a769603e2b65db7d408e1ecfe8b6e5
sameer <sameersaini40@gmail.com>
2018-10-05 23:43:12 -0600

    minor code cleaning

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,3 @@
 export const FETCH_USER = 'FETCH_USER';
-export const FETCH_STATS = 'FETCH_STATS';
 export const FETCH_USER_REPOS = 'FETCH_USER_REPOS';
 export const FETCH_USER_ERROR = 'FETCH_USER_ERROR';
